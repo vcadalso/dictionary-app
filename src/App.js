5605a8ba0df3ce6c5078b4d56135d94b0d67bf1e
vcadalso <vanessa.cadalso@gmail.com>
2024-06-20 10:46:58 -0400

    Added functional search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo4.png';
 import './App.css';
+import Dictionary from './Dictionary';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
-       
-      </header>
+       </header>
+       <main>
+        <Dictionary />
+       </main>
+       <footer className="app-footer"> <small> Coded by Vanessa Cadalso</small></footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
