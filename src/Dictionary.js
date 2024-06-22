0cafdf8c2172c7a12716143d82d63ac514ac1f8f
vcadalso <vanessa.cadalso@gmail.com>
2024-06-22 14:26:00 -0400

    Integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
+        
+
+        let apiKey = "0300e14043fb59596a12t1bb17o183cf";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
