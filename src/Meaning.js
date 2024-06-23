7125d72762a5b2fd4181ad08d30d8cc5ddc1b2c0
vcadalso <vanessa.cadalso@gmail.com>
2024-06-23 12:21:21 -0400

    Fixed phonetic bug

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,7 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-    console.log(props.meaning);
+   
     return (
         <div className="meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
--- src/Results.js
+++ src/Results.js
@@ -7,13 +7,7 @@ export default function Results(props) {
         return (
           <div className="results">
             <h2>{props.results.word}</h2>
-            {props.results.phonetics.map(function(phonetic, index) {
-                return (
-                    <div key={index}>
-                        <Phonetic phonetic={phonetic} />
-                    </div>
-                );
-            })}
+            <Phonetic phonetic={props.phonetic} />
             
             {props.results.meanings.map(function(meaning, index) {
                 return (
