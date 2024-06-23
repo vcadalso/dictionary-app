8d832d3e0a680d8fbf7de0291fef14f245b3b3a0
vcadalso <vanessa.cadalso@gmail.com>
2024-06-23 10:45:22 -0400

    Fixed bug

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,18 +5,13 @@ export default function Meaning(props) {
     return (
         <div className="meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definitions.map(function (definition, index) {
-                return (
-                    <div key={index}>
+                    <div>
                         <p>
-                            {definition.definition}
+                            {props.meaning.definition}
                             <br />
-                            <em>{definition.example}</em>
+                            <em>{props.meaning.example}</em>
                         </p>
                     </div>
-                );
-            })}
-            
         </div>
     );
 }
