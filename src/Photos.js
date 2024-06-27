1117538011bd3416940f42470deb12b05c7fa627
vcadalso <vanessa.cadalso@gmail.com>
2024-06-27 17:40:21 -0400

    fixed bug

--- src/Photos.js
+++ src/Photos.js
@@ -9,8 +9,8 @@ export default function Photos(props) {
                 {props.photos.map(function (photo, index) {
                     return (
                     <div className="col-4" key={index}>
-                    <a href={photo.src.original} target="_blank">
-                    <img src={photo.src.landscape} className="img-fluid" />
+                    <a href={photo.src.original} rel="noreferrer" target="_blank">
+                    <img alt="landscape" src={photo.src.landscape} className="img-fluid" />
                     </a>
                     </div>
                     );
